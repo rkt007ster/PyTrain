3bb2bb276ae578fb5ce98b420c5ffa2b0340f6f0
rkt007ster <64979008+rkt007ster@users.noreply.github.com>
2025-04-01 21:17:16 +0530

    Add files via upload

---
+++ Task_2.py
@@ -0,0 +1,9 @@
+
+#Task 2: Create a Personalized Greeting
+
+def __main__():
+    fname = input('Enter your First name: ')
+    lname = input('Enter your Last name: ')
+    print(f'Hello {fname} {lname}! Welcome to the Python program.')
+
+__main__()
